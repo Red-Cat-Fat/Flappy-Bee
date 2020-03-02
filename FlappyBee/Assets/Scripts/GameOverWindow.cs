123305c27969b87212114f30c79eb63e5f8c8baa
Andrey Boronnikov <boronnikov_a_i@mail.ru>
2020-03-03 02:42:52 +0300

    Сделал компонент для окна конца игры

---
+++ FlappyBee/Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverWindow : MonoBehaviour
+{
+		[SerializeField]
+		private Text _scoreText;
+
+		public void SetScore(int score)
+		{
+				_scoreText.text = $"Total score: {score}";
+		}
+
+		public void OnRestartButtonClick()
+		{
+				SceneManager.LoadScene("Main", LoadSceneMode.Single);
+		}
+}
